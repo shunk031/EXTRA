c3f23c8ce4a46d67ea5984045557008e76f32ccf
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2021-09-15 23:58:49 +0900

    apply formatter

--- format_amazon.py
+++ format_amazon.py
@@ -3,23 +3,27 @@ import pickle
 import gzip
 
 
-raw_path = 'reviews_Movies_and_TV_5.json.gz'  # path to load raw reviews
-review_path = 'reviews.pickle'  # path to save reviews
+raw_path = "reviews_Movies_and_TV_5.json.gz"  # path to load raw reviews
+review_path = "reviews.pickle"  # path to save reviews
 
 reviews = []
-for line in gzip.open(raw_path, 'r'):
+for line in gzip.open(raw_path, "r"):
     review = eval(line)
-    text = ''
-    if 'summary' in review:
-        summary = review['summary']
-        if summary != '':
-            text += summary + '\n'
-    text += review['reviewText']
+    text = ""
+    if "summary" in review:
+        summary = review["summary"]
+        if summary != "":
+            text += summary + "\n"
+    text += review["reviewText"]
 
-    json_doc = {'user': review['reviewerID'],
-                'item': review['asin'],
-                'rating': int(review['overall']),
-                'text': text,
-                'time': datetime.fromtimestamp(int(review['unixReviewTime'])).strftime('%Y-%m-%d')}
+    json_doc = {
+        "user": review["reviewerID"],
+        "item": review["asin"],
+        "rating": int(review["overall"]),
+        "text": text,
+        "time": datetime.fromtimestamp(int(review["unixReviewTime"])).strftime(
+            "%Y-%m-%d"
+        ),
+    }
     reviews.append(json_doc)
-pickle.dump(reviews, open(review_path, 'wb'))
+pickle.dump(reviews, open(review_path, "wb"))
--- process_sentence.py
+++ process_sentence.py
@@ -3,13 +3,13 @@ import nltk
 import re
 
 
-review_path = 'reviews.pickle'  # resulting from format_amazon.py
-sentence_path = 'sentences.pickle'  # path to save sentences
+review_path = "reviews.pickle"  # resulting from format_amazon.py
+sentence_path = "sentences.pickle"  # path to save sentences
 
 
 def get_sentences(string):
-    string = re.sub('[:,?!\n]', '.', string)
-    sentences = [sent.strip() for sent in string.split('.') if sent.strip() != '']
+    string = re.sub("[:,?!\n]", ".", string)
+    sentences = [sent.strip() for sent in string.split(".") if sent.strip() != ""]
     return sentences
 
 
@@ -29,25 +29,25 @@ def get_sentence_attr(string):
     return len(words), subj_num, noun_num, adj_num
 
 
-subj_words = ['i', 'me', 'my', 'mine', 'myself', 'we', 'us', 'our', 'ours', 'ourselves']
-noun_taggers = ['NN', 'NNP', 'NNPS', 'NNS']
-adj_taggers = ['JJ', 'JJR', 'JJS']
+subj_words = ["i", "me", "my", "mine", "myself", "we", "us", "our", "ours", "ourselves"]
+noun_taggers = ["NN", "NNP", "NNPS", "NNS"]
+adj_taggers = ["JJ", "JJR", "JJS"]
 
 
-reviews = pickle.load(open(review_path, 'rb'))
+reviews = pickle.load(open(review_path, "rb"))
 sentences = []
 for idx, review in enumerate(reviews):
-    text = review['text']
+    text = review["text"]
     exps = get_sentences(text)
     for exp in exps:
         word_n, subj_n, noun_n, adj_n = get_sentence_attr(exp)
         sentence = {
-            'review_idx': idx,
-            'exp': exp,
-            'word_num': word_n,
-            'subj_num': subj_n,
-            'noun_num': noun_n,
-            'adj_num': adj_n,
+            "review_idx": idx,
+            "exp": exp,
+            "word_num": word_n,
+            "subj_num": subj_n,
+            "noun_num": noun_n,
+            "adj_num": adj_n,
         }
         sentences.append(sentence)
-pickle.dump(sentences, open(sentence_path, 'wb'))
+pickle.dump(sentences, open(sentence_path, "wb"))
