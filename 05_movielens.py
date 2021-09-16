676994795645c21c999396d0fbd2079f6f7ab723
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2021-09-17 01:15:33 +0900

    update

--- 05_movielens.py
+++ 05_movielens.py
@@ -1,38 +1,87 @@
-from datetime import datetime
-import pickle
+import argparse
 import json
+import pathlib
+import pickle
+from datetime import datetime
 
 
-ID_path = "IDs.pickle"  # resulting from keep_valid.py
-id2exp_path = "id2exp.json"  # resulting from keep_valid.py
-ID_txt_path = "IDs.txt"  # path to save explanation IDs in plain text
-id2exp_txt_path = "id2exp.txt"  # path to save id2exp in plain text
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--id-path",
+        type=pathlib.Path,
+        default=pathlib.Path(__file__).resolve().parent / "outputs" / "ids.pickle",
+        help="resulting from `keep_valid.py`",
+    )
+    parser.add_argument(
+        "--id2exp-path",
+        type=pathlib.Path,
+        default=pathlib.Path(__file__).resolve().parent / "outputs" / "id2exp.json",
+        help="resulting from `keep_valid.py`",
+    )
+    parser.add_argument(
+        "--id-txt-path",
+        type=pathlib.Path,
+        default=pathlib.Path(__file__).resolve().parent / "outputs" / "ids.txt",
+        help="path to save explanation IDs in plain text",
+    )
+    parser.add_argument(
+        "--id2exp-txt-path",
+        type=pathlib.Path,
+        default=pathlib.Path(__file__).resolve().parent / "outputs" / "id2exp.txt",
+        help="path to save id2exp in plain text",
+    )
+
+    return parser.parse_args()
 
 
-IDs = pickle.load(open(ID_path, "rb"))
-lines = []
-for record in IDs:
-    # userID::itemID::rating::timestamp::expID:expID::oexpID:oexpID
-    user = record["user"]
-    item = record["item"]
-    rating = record["rating"]
-    time = int(datetime.strptime(record["time"], "%Y-%m-%d").timestamp())
-    exp_idx = record["exp_idx"]
-    oexp_idx = record["oexp_idx"]
-    exp = ":".join(exp_idx)
-    oexp = ":".join(oexp_idx)
-    line = "::".join([user, item, str(rating), str(time), exp, oexp])
-    lines.append(line)
-with open(ID_txt_path, "w", encoding="utf-8") as f:
-    f.write("\n".join(lines))
+def movielens(
+    id_path: pathlib.Path,
+    id_txt_path: pathlib.Path,
+    id2exp_path: pathlib.Path,
+    id2exp_txt_path: pathlib.Path,
+) -> None:
+
+    with open(id_path, "rb") as rf:
+        IDs = pickle.load(rf)
+
+    lines = []
+    for record in IDs:
+        # userID::itemID::rating::timestamp::expID:expID::oexpID:oexpID
+        user = record["user"]
+        item = record["item"]
+        rating = record["rating"]
+        time = int(datetime.strptime(record["time"], "%Y-%m-%d").timestamp())
+        exp_idx = record["exp_idx"]
+        oexp_idx = record["oexp_idx"]
+        exp = ":".join(exp_idx)
+        oexp = ":".join(oexp_idx)
+        line = "::".join([user, item, str(rating), str(time), exp, oexp])
+        lines.append(line)
+
+    with open(id_txt_path, "w", encoding="utf-8") as f:
+        f.write("\n".join(lines))
+
+    with open(id2exp_path, "r", encoding="utf-8") as f:
+        id2exp = json.load(f)
+    lines = []
+    for (idx, exp) in id2exp.items():
+        # expID::exp
+        line = "::".join([idx, exp])
+        lines.append(line)
+    with open(id2exp_txt_path, "w", encoding="utf-8") as f:
+        f.write("\n".join(lines))
 
 
-with open(id2exp_path, "r", encoding="utf-8") as f:
-    id2exp = json.load(f)
-lines = []
-for (idx, exp) in id2exp.items():
-    # expID::exp
-    line = "::".join([idx, exp])
-    lines.append(line)
-with open(id2exp_txt_path, "w", encoding="utf-8") as f:
-    f.write("\n".join(lines))
+def main() -> None:
+    args = parse_args()
+    movielens(
+        id_path=args.id_path,
+        id_txt_path=args.id_txt_path,
+        id2exp_path=args.id2exp_path,
+        id2exp_txt_path=args.id2exp_txt_path,
+    )
+
+
+if __name__ == "__main__":
+    main()
